d57a7517ee7ab8daf0c6c160b675e3b269b2b20d
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-26 22:28:09 +0100

    request: post

--- Api/serializers.py
+++ Api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from .models import Poll
+from .models import Poll, Answer
 
 class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
         fields = '__all__'
 
-# class AnswerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Answer
-#         fields = '__all__'
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
--- Api/views.py
+++ Api/views.py
@@ -11,3 +11,9 @@ def poll_list(request):
         polls = Poll.objects.all()
         serializer = PollSerializer(polls, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = PollSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
