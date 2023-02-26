0a810dd118b0ac27e0628778e02ace3c245dd99e
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-26 20:26:50 +0100

    routes

--- Api/models.py
+++ Api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=250)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question
+ 
+class Answer(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.answer_text
+    
