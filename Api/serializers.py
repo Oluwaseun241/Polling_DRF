d5c814771cebcbaf00848a12d8075ec2e0e30627
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-26 21:42:02 +0100

    serializers

--- Api/serializers.py
+++ Api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import Poll
 
-class PollSerializers(serializers.ModelSerializers):
+class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
         fields = '__all__'
 
-class AnswerSerializers(serializers.ModelSerializers):
-    class Meta:
-        model = Answer
-        fields = '__all__'
+# class AnswerSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Answer
+#         fields = '__all__'
--- Api/views.py
+++ Api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Poll
+from .serializers import PollSerializer
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def poll_list(request):
+    if request.method == 'GET':
+        polls = Poll.objects.all()
+        serializer = PollSerializer(polls)
+        return Response(serializer.data)
