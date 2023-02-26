3fd5cf49d784e2f0023c486b14374bf692d225fc
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-26 20:42:20 +0100

    serializers

--- Api/serializers.py
+++ Api/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Poll
 
-class PollSerializers(serializers.Serializers):
-    id = model.IntegerField(read_only=True)
-    question = models.CharField(max_length=250)
+class PollSerializers(serializers.ModelSerializers):
+    class Meta:
+        model = Poll
+        fields = '__all__'
 
-class AnswerSerializers(serializers.Serializers):
-    pass
+class AnswerSerializers(serializers.ModelSerializers):
+    class Meta:
+        model = Answer
+        fields = '__all__'
