651fe9cb279f2116ed8cae4ef8cf9727e987c42d
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-27 11:38:48 +0100

    views:class base

--- Api/urls.py
+++ Api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.poll_list)
+    path('poll/', views.PollList),
+    path('poll/<int:pk>', views.PollDetail)
+    #path('', views.poll_list)
 ]
--- Api/views.py
+++ Api/views.py
@@ -1,19 +1,31 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Poll
 from .serializers import PollSerializer
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def poll_list(request):
-    if request.method == 'GET':
-        polls = Poll.objects.all()
-        serializer = PollSerializer(polls, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = PollSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+PollList = PollList.as_view()
+
+class PollDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+PollDetail = PollDetail.as_view()
+
+# Function based views
+# @api_view(['GET', 'POST'])
+# def poll_list(request):
+#     if request.method == 'GET':
+#         polls = Poll.objects.all()
+#         serializer = PollSerializer(polls, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = PollSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
