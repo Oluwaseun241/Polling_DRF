8b3477903dd7cadfaa9ae220ec8efeb72bea38be
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-28 14:50:31 +0100

    ompo

--- Api/urls.py
+++ Api/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
-    path('', views.registration_view, name='user_create'),
+    path('register/', views.registration_view, name='user_create'),
     path('login/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('poll/', views.PollList),
