aa78b045ae719ee3e7c27fd88f783936c3b77db5
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-03-06 20:02:03 +0100

    feat:final

--- Api/serializers.py
+++ Api/serializers.py
@@ -15,8 +15,6 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
   
-
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'password2')
@@ -37,7 +35,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
-        fields = ['id','answer_text', 'poll_id']
+        fields = ['answer_text', 'poll_id']
     
 
 class PollCreateSerializer(serializers.ModelSerializer): 
